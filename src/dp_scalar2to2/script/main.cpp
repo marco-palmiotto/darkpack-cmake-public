02788e64741397725392e74f63aea2f6584fcea2
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-09-19 10:49:22 +0000

    added I output

--- src/dp_scalar2to2/script/main.cpp
+++ src/dp_scalar2to2/script/main.cpp
@@ -1,9 +1,10 @@
 #include "scalar2to2.h"
-#include "config.h"
+#include "config.hpp"
 using namespace scalar2to2;
 
 int main(int argn, const char ** args) {
     std::cout << "dp_scalar2to2 version " << dp_scalar2to2_VERSION_MAJOR << '.' << dp_scalar2to2_VERSION_MINOR << '\n';
     std::cout << "Running " << args[0]<< " with " << argn << " argument(s).\n";
+    std::cout << "I=" << I << '\n';
     return 0;
 }

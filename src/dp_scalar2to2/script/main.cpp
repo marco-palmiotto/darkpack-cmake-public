e1d4e82ac11ada61dd85b4c5f2e2963a74005a5c
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-09-19 11:43:37 +0000

    Renamed MathLib to DPMathLib

--- src/dp_scalar2to2/script/main.cpp
+++ src/dp_scalar2to2/script/main.cpp
@@ -1,9 +1,9 @@
-#include "scalar2to2.h"
 #include "config.hpp"
+#include "params_new.hpp"
 using namespace scalar2to2;
 
 int main(int argn, const char ** args) {
-    std::cout << "dp_scalar2to2 version " << __SPEC_LIB_NAME__VERSION_MAJOR << '.' << __SPEC_LIB_NAME__VERSION_MINOR << '\n';
+    std::cout << "DarkPack version " << __SPEC_LIB_NAME__VERSION_MAJOR << '.' << __SPEC_LIB_NAME__VERSION_MINOR << '\n';
     std::cout << "Running " << args[0]<< " with " << argn << " argument(s).\n";
     std::cout << "I=" << I << '\n';
     return 0;

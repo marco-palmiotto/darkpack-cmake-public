df577b339a9ad7bacbaed1c12b2a96de4c8c56a0
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-10-02 13:15:55 +0000

    updated to limit resource consumption and prevent crashes

--- build.sh
+++ build.sh
@@ -1,4 +1,26 @@
-#! /bin/sh
+#! /bin/bash
 
-cd build 
-ninja
+# old option was -l1 -k2 works immediately, but very slow
+ninja -C build -l2 -j4
+exit_code=$?
+
+if [[ !$exit_code -ne 0 ]]
+then
+    echo "First build failed"
+    nthreads=4
+    avgload=2
+    nmaxfailures=0 # 0 is infinity
+fi
+
+while [[ $exit_code -ne 0 && $nthreads -ne 0 ]]
+do
+    echo "Build failed, trying with less threads"
+    echo "Running: ninja -C build -l$avgload -k$nmaxfailures -j$nthreads"
+    ninja -C build -l$avgload -k$nmaxfailures -j$nthreads
+    exit_code=$?
+
+    [ $avgload -ge 2 ]  && avgload=$(($avgload-1))
+    nthreads=$(($nthreads-1))
+done
+
+[[ !$exit_code ]] && echo "Building failed" || echo "Build succeded"

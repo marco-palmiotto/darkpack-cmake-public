14cf65daa7aed69a76e2f95e6f271278f6aeb9f7
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-10-04 10:23:00 +0000

    inserted control system for multithreading

--- build.sh
+++ build.sh
@@ -1,26 +1,23 @@
 #! /bin/bash
 
+nthreads=4
+avgload=3
+nmaxfailures=2 # 0 is infinity
+
 # old option was -l1 -k2 works immediately, but very slow
-ninja -C build -l1 
+ninja -C build -l$avgload -k$nmaxfailures
 exit_code=$?
 
-if [[ !$exit_code -ne 0 ]]
-then
-    echo "First build failed"
-    nthreads=4
-    avgload=2
-    nmaxfailures=0 # 0 is infinity
-fi
+echo exit_code=$exit_code
 
 while [[ $exit_code -ne 0 && $nthreads -ne 0 ]]
 do
     echo "Build failed, trying with less threads"
+    [ $avgload -ge 2 ]  && avgload=$(($avgload-1))
+    nthreads=$(($nthreads-1))
     echo "Running: ninja -C build -l$avgload -k$nmaxfailures -j$nthreads"
     ninja -C build -l$avgload -k$nmaxfailures -j$nthreads
     exit_code=$?
-
-    [ $avgload -ge 2 ]  && avgload=$(($avgload-1))
-    nthreads=$(($nthreads-1))
 done
 
-[[ !$exit_code ]] && echo "Building failed" || echo "Build succeded"
+[[ $exit_code -ne 0 ]] && echo "Building failed" || echo "Build succeded"

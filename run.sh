febfac4f02412ac739e3b5db979d3883eb7fa4fa
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-10-04 14:08:19 +0000

    runs the scan

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
-./build/src/Test/scalar_test.x src/models/scalar.lha
+# Testing the scalar model
+# ./build/src/Test/scalar_test.x src/models/scalar.lha
+
+# Debugging the scalar model
+./build/src/Test/scalar_scan_2.x src/models/scalar.lha

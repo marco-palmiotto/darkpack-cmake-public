6d6c237756d5e8d4162724cbbd0f154d1c991ecd
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-10-16 13:44:00 +0000

    Added global command that automatically installs the headers of the desired libraries

--- configure.sh
+++ configure.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-libraries_list=(DPMathLib dp_scalar2to2)
-
 cmake -S . -B ./build \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug \
       -DCMAKE_INSTALL_PREFIX=./build/.install \
@@ -9,7 +7,4 @@ cmake -S . -B ./build \
 
 cd build
 
-for elem in ${libraries_list[@]}
-do
-      cmake --build . --target install_headers_$elem
-done
+cmake --build . --target install_headers_all

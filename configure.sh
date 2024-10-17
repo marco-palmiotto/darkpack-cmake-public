0d32c0573342cf851b8a84e86e162ae26729ab61
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-10-17 09:32:57 +0000

    fixed syntax error in vscode tasks

--- configure.sh
+++ configure.sh
@@ -4,7 +4,5 @@ cmake -S . -B ./build \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug \
       -DCMAKE_INSTALL_PREFIX=./build/.install \
       -G Ninja
-
-cd build
-
-cmake --build . --target install_headers_all
+      
+cmake --build ./build --target install_headers_all

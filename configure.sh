c7f8ab7cd346ecec93463d4d4d6ba75f60d4b59a
PALMIOTTO MARCO p2026239 <marco.palmiotto@univ-lyon1.fr>
2024-10-18 11:55:44 +0200

    switched to release mode

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cmake -S . -B ./build \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=./build/.install \
       -G Ninja
       

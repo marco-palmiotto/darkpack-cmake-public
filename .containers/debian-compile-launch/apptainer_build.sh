3c3d3c4264c9990c8485d99f6ace380ac63af71f
marco-palmiotto <marco.palmiotto@ik.me>
2024-10-21 17:23:01 +0200

    Added script that automatises Dockerfile->sif conversion

---
+++ .containers/debian-compile-launch/apptainer_build.sh
@@ -0,0 +1,10 @@
+# Limita la memoria virtuale a 8GB
+ulimit -v 4000000
+
+# Limita il numero di processi a 100
+ulimit -u 10
+
+# Limita l'utilizzo della CPU a 10 minuti
+ulimit -t 600
+
+sudo apptainer build darkpack-launch.tar darkpack-launch.def

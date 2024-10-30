de61b67ca4b24664221fe8a499951d749b4bcb41
marco <marco.palmiotto94@gmail.com>
2024-10-30 10:43:11 +0100

    fixed cli call to podman

--- .containers/debian-compile-launch/build_and_convert.sh
+++ .containers/debian-compile-launch/build_and_convert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 podman build -t darkpack-launch:latest .
-# podman save -o darkpack-launch.tar darkpack-launch:latest
-#&& sudo apptainer build darkpack-launch.sif docker-archive://darkpack-launch.tar
+podman save --format oci-archive -o darkpack-launch.tar localhost/darkpack-launch:latest
+sudo apptainer build darkpack-launch.sif darkpack-launch.tar
+scp darkpack-launch.sif mpalmiot@cca.in2p3.fr:/pbs/home/m/mpalmiot/code/

c6d8d5f22a84af2e5383d93754668476c87572b5
marco-palmiotto <marco.palmiotto@univ-lyon1.fr>
2024-12-09 18:59:10 +0100

    removed config.hpp self-generated

--- src/dp_mssm2to2/config.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define __SPEC_LIB_NAME__               mssm2to2
-#define __SPEC_LIB_NAME__VERSION_MAJOR  1
-#define __SPEC_LIB_NAME__VERSION_MINOR  0
-#include "mssm2to2.h"
-constexpr const char * OUTPATH = "/workspaces/darkpack-cmake/out/";

a5c3e118b28ff0a478e335df38194e192826e6a0
marco-palmiotto <marco.palmiotto@ik.me>
2024-12-10 08:27:15 +0000

    fixed bug number arguments

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
 ./build/src/main/su2_scan_cma-single-1-dim.x \
-    src/models/su2.lha \
-    out/scans/out_single_1dim.out \
-    0.1 \
-    0.33 \
-    2.0
+ src/models/su2.lha \
+ out/scans/su2f_result_0.33333_30.out \
+ 0.01 \
+ 0.33333 \
+ 1.0  \
+ 1.0e+5 \
+ 50 \
+ 17

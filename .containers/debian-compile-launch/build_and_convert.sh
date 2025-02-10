8028bc8a02b47117104af139b53579640ca5fce3
marco-palmiotto <marco.palmiotto@ik.me>
2025-02-10 13:45:09 +0000

    renamed the dockerfiles to create separate containers for dev and CI

--- .containers/debian-compile-launch/build_and_convert.sh
+++ .containers/debian-compile-launch/build_and_convert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-podman build -t darkpack-launch:latest .
-podman save --format oci-archive -o darkpack-launch.tar localhost/darkpack-launch:latest
-sudo apptainer build darkpack-launch.sif darkpack-launch.tar
-scp darkpack-launch.sif mpalmiot@cca.in2p3.fr:/pbs/home/m/mpalmiot/code/
+name_output=darkpack-launch-debian
+
+podman build -t $name_output:latest ./Dockerfile-debian-launch && \
+podman save --format oci-archive -o $name_output.tar localhost/$name_output:latest
+# sudo apptainer build $name_output.sif $name_output.tar

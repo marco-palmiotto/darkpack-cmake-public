1808b51d339b296808cbdabb539634daf3259be7
marco-palmiotto <marco.palmiotto@ik.me>
2025-02-11 13:07:20 +0000

    set up podman for devconatiners

---
+++ .devcontainer/build_and_push_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+distros=( "fedora36" )
+
+for distro in ${distros[@]}
+do
+    echo Working on $distro
+    name_output=darkpack-launch-$distro
+
+    podman build -t $name_output:latest -f Dockerfile-${distro}-launch
+
+    podman tag localhost/${name_output} docker.io/marcopalmiotto/darkpack-deploy:$distro
+
+    podman push docker.io/marcopalmiotto/darkpack-deploy:$distro
+done
+
+
+# podman save --format oci-archive -o $name_output.tar localhost/$name_output:latest
+# sudo apptainer build $name_output.sif $name_output.tar

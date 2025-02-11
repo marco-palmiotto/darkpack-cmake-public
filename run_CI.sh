ce8fd33de3e7158d7b9dd7bb5b1cef5397df0967
marco-palmiotto <marco.palmiotto@ik.me>
2025-02-11 13:47:20 +0000

    set up script for CI

---
+++ run_CI.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Configuring cmake"
+cmake -S . -B ./build \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=./build/.install \
+      -DENABLE_CLANG_FORMAT=ON \
+      -G Ninja || exit 1
+
+
+echo "Running format check"
+cmake --build ./build --target format-check || exit 1
+
+echo "Installing headers"
+cmake --build ./build --target install_headers_all  || exit 1
+
+echo "Building documentation"
+cmake --build ./build --target darkpack_doc || exit 1
+
+echo "Building darkpack"
+ninja -C build || exit 1

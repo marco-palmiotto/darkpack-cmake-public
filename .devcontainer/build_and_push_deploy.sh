746817e1bdad38373e1b7208d3f9849fb35b7727
marco-palmiotto <marco.palmiotto@ik.me>
2025-02-13 11:32:29 +0000

    Polishing folders

--- .devcontainer/build_and_push_deploy.sh
+++ .devcontainer/build_and_push_deploy.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 distros=( "fedora36" "debian" )
+suffix=deploy
 
 for distro in ${distros[@]}
 do
     echo Working on $distro
-    name_output=darkpack-launch-$distro
+    name_output=darkpack-$suffix-$distro
 
-    podman build -t $name_output:latest -f Dockerfile-${distro}-launch
+    podman build -t $name_output:latest -f Dockerfile-${distro}-$suffix
 
     podman tag localhost/${name_output} docker.io/marcopalmiotto/darkpack-deploy:$distro
 
     podman push docker.io/marcopalmiotto/darkpack-deploy:$distro
+
+    # To save the image in a sif file
+    # podman save --format oci-archive -o $name_output.tar localhost/$name_output:latest
+    # sudo apptainer build $name_output.sif $name_output.tar
 done
 
 
-# podman save --format oci-archive -o $name_output.tar localhost/$name_output:latest
-# sudo apptainer build $name_output.sif $name_output.tar
+

2ac85d67a95d3be8cd743b2078e1eabe41e040c1
marco-palmiotto <marco.palmiotto@ik.me>
2025-04-23 09:33:01 +0000

    target "format" is now executed before all others

--- configure.sh
+++ configure.sh
@@ -3,12 +3,12 @@
 cmake -S . -B ./build \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug \
       -DCMAKE_INSTALL_PREFIX=./build/.install \
-      -DMODELS_LIBS_NAMES="dp_scalar2to2;dp_su2f2to2;dp_u1f2to2" \
       -DENABLE_CLANG_FORMAT=ON \
-      -DENABLE_SUBMODULE_CMA=ON \
-      -G Ninja
+      -DENABLE_SUBMODULE_CMA=OFF \
+      -G Ninja \
+      -DMODELS_LIBS_NAMES="dp_u1f2to2"
 
-cmake --build ./build --target format format-check install_headers_all darkpack_doc # && \
-# cmake --build ./build --target format-check && \
+cmake --build ./build --target format && \
+cmake --build ./build --target format-check install_headers_all darkpack_doc #&& \
 # cmake --build ./build --target install_headers_all 
 # cmake --build ./build --target darkpack_doc 

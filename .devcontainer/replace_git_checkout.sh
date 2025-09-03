11e33e91152afc363c2688791ca82abc460d5997
marco-palmiotto <marco.palmiotto@ik.me>
2025-09-03 14:49:37 +0000

    updated containers to support the version of MARTY with separate group_g declarations

--- .devcontainer/replace_git_checkout.sh
+++ .devcontainer/replace_git_checkout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -exec sed -i 's/git checkout dac95a471e0c7ea12f18ba13df0a7383406fdd16/git checkout dac95a471e0c7ea12f18ba13df0a7383406fdd16/g' {} +
+find . -type f -exec sed -i 's/git checkout dac95a471e0c7ea12f18ba13df0a7383406fdd16/git checkout separating_global/g' {} +
--- src/dp_u1f2to2/smBsm.hpp
+++ src/dp_u1f2to2/smBsm.hpp
@@ -1,27 +1,6 @@
 inline const int SIZEPHYSICALSM = 19;
 inline const int SIZEPHYSICALBSM = 1;
 inline const int TOTAL_PARTICLES = 20;
-inline const std::array<int, SIZEPHYSICALBSM> bsm_particles{
-  chi_dm_1
-};
-inline const std::array<int,SIZEPHYSICALSM> sm_particles{
-  G,
-  V_3,
-  phi,
-  W,
-  h,
-  A,
-  Z,
-  nu_e,
-  nu_mu,
-  nu_tau,
-  e,
-  mu,
-  tau,
-  u,
-  c,
-  t,
-  d,
-  s,
-  b
-};
+inline const std::array<int, SIZEPHYSICALBSM> bsm_particles{chi_dm_1};
+inline const std::array<int, SIZEPHYSICALSM> sm_particles{G, V_3, phi, W, h, A, Z, nu_e, nu_mu, nu_tau,
+                                                          e, mu,  tau, u, c, t, d, s,    b};
